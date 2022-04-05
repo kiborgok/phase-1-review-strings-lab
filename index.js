2ba1721a5d40ec4e08eadcb1e1e4b1b701654a2b
Alex Kiborgok <borgokalek14@gmail.com>
2022-04-05 20:21:54 +0300

    display messages

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+const currentUser = "Vanellope";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const upperCaseWelcomeMsg = welcomeMessage.toUpperCase();
+
+const excitedWelcomeMessage = upperCaseWelcomeMsg;
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`
